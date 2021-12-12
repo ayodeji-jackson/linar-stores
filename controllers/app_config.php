1578ae548450ba8c9150fcb0fee7b18352787c1a
jacksonemmanuel <71940927+jacksonemmanuel@users.noreply.github.com>
2021-12-12 01:53:58 +0100

    Update app_config.php
    Converted MySQL to PostgreSQL

--- controllers/app_config.php
+++ controllers/app_config.php
@@ -1,35 +1,36 @@
 <?php
 
 class Database {
-    private $mysqli;
+    private $dbObj;
 
     function __construct() {
         try {
-            $this->mysqli = new mysqli('localhost', 'root', '', 'linarStores');
-        } catch(Exception $e) {
+            $db = parse_url(getenv("DATABASE_URL"));
+
+            $dbObj = new PDO("pgsql:" . sprintf(
+                "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                $db["host"],
+                $db["port"],
+                $db["user"],
+                $db["pass"],
+                ltrim($db["path"], "/")
+            ));
+        } catch(PDOException $e) {
             return $e->getMessage();
+            die();
         }
     }
     function read($tableName, $id, $extra_param = "") {
-        return $this->mysqli->query("SELECT * FROM $tableName WHERE $tableName" . "_Id = $id $extra_param")->fetch_assoc();
+        return pg_fetch(pg_query($this->dbObj, "SELECT * FROM $tableName WHERE $tableName" . "_Id = $id $extra_param"));
     }
     function readAll($tableName, $extra_param = "") {
-        return $this->mysqli->query("SELECT * FROM $tableName $extra_param")->fetch_all(MYSQLI_ASSOC);
-    } 
-    function insert($sql) {
-
-    }
-    function update($sql) {
-
-    }
-    function delete($sql) {
-
+        return pg_fetch_all(pg_query($this->dbObj, "SELECT * FROM $tableName $extra_param"), PGSQL_ASSOC);
     }
     function search($tableName, $column, $value, $extra_param = "") {
-        return $this->mysqli->query("SELECT * FROM $tableName WHERE MATCH($tableName" . "_$column) AGAINST('$value') $extra_param")->fetch_all(MYSQLI_ASSOC);
+        return pg_fetch_all(pg_query($this->dbObj, "SELECT * FROM $tableName WEHRE to_tsvector($column) @@ to_tsquery($value) $extra_param"), PGSQL_ASSOC);
     }
     function __destruct() {
-        $this->mysqli->close();
+        pg_close($this->dbObj);
     }
 }
 
