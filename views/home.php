139f7211fb705e88318c07d86cbe964ee921a1a3
jacksonemmanuel <71940927+jacksonemmanuel@users.noreply.github.com>
2021-12-12 13:45:20 +0100

    Update home.php

--- views/home.php
+++ views/home.php
@@ -6,7 +6,7 @@ $category_array = $category_table->readAll("linarStores.Category");
 print_r($category_array);
 unset($category_array);
 $product_table = new Database;
-// $product_array_recent = $product_table->readAll("Product", "ORDER BY `Product_Date` DESC LIMIT 5");
+$product_array_recent = $product_table->readAll("linarStores.Product", "ORDER BY `Product_Date` DESC LIMIT 5");
 
 ?>
 
@@ -29,10 +29,10 @@ $product_table = new Database;
 
             foreach ($category_array as $category) {
                 echo "<div class='category cell-carousel'>
-                        <img class='category-image' alt='" . $category['Category_Name'] . "' src='/images/" . $category['Category_Image_URL'] . "' />
-                        <h3>" . $category['Category_Name'] . "</h3>
-                        <p>" . $category['Category_Description'] . "</p>
-                        <a href='" . strtolower($category['Category_Name']) . "' class='btn category-button btn-primary'><span class='button-text'>Shop Now</span></a>
+                        <img class='category-image' alt='" . $category['category_name'] . "' src='/images/" . $category['category_image_url'] . "' />
+                        <h3>" . $category['category_name'] . "</h3>
+                        <p>" . $category['category_description'] . "</p>
+                        <a href='" . strtolower($category['category_name']) . "' class='btn category-button btn-primary'><span class='button-text'>Shop Now</span></a>
                     </div>";
             }
             $category_array = NULL;
@@ -44,29 +44,29 @@ $product_table = new Database;
             <?php
 
             foreach ($product_array_recent as $product) {
-                $amount_saved = $product['Product_ActualPrice'] - $product['Product_DiscountPrice'];
-                echo "<div class='product cell-carousel' data-product-id='" . $product['Product_Id'] . "'>
+                $amount_saved = $product['product_actualprice'] - $product['product_discountprice'];
+                echo "<div class='product cell-carousel' data-product-id='" . $product['product_id'] . "'>
                         <div class='product-savings'><i class='fa fa-percent'></i> Save <span class='price'>$amount_saved</span></div>
-                        <img class='product-image' alt='" . $product['Product_Name'] . "' 
-                        src='/images/" . $product['Product_Image_URL'] . "' />
-                        <h3 class='product-category'><a href='" . strtolower($product['Product_Category']) . "'>" . $product['Product_Category'] . "</a></h3>
-                        <p class='product-name'>" . $product['Product_Name'] . "</p>
-                        <div class='rating' data-rating='" . $product['Product_Rating'] . "'>
+                        <img class='product-image' alt='" . $product['product_name'] . "' 
+                        src='/images/" . $product['product_image_url'] . "' />
+                        <h3 class='product-category'><a href='" . strtolower($product['product_category']) . "'>" . $product['product_category'] . "</a></h3>
+                        <p class='product-name'>" . $product['product_name'] . "</p>
+                        <div class='rating' data-rating='" . $product['product_rating'] . "'>
                             <i class='fa fa-star grey-star'></i>
                             <i class='fa fa-star grey-star'></i>
                             <i class='fa fa-star grey-star'></i>
                             <i class='fa fa-star grey-star'></i>
                             <i class='fa fa-star grey-star'></i>
-                            <span class='rating-number'>" . $product['Product_Rating'] . "</span>
+                            <span class='rating-number'>" . $product['product_rating'] . "</span>
                             <span class='rating-users'></span>
                         </div>
                         <div class='price-container'>
-                            <span class='price product-discount-price'>" . $product['Product_DiscountPrice'] . "</span>
-                            <span class='price product-actual-price'>" . $product['Product_ActualPrice'] . "</span>
+                            <span class='price product-discount-price'>" . $product['product_discountprice'] . "</span>
+                            <span class='price product-actual-price'>" . $product['Product_actualprice'] . "</span>
                         </div>
                         <div class='progressbar-container'>
-                            <div class='progressbar' role='progressbar' aria-valuemin='0' aria-valuemax='100' aria-valuenow='" . $product['Product_Qty'] . "'></div>";
-                $available_in_stock = $product['Product_Qty'] > 1 ? "<span class='num-qty-available'>" . $product['Product_Qty'] . "</span> available in stock" : "Last product in stock";
+                            <div class='progressbar' role='progressbar' aria-valuemin='0' aria-valuemax='100' aria-valuenow='" . $product['product_qty'] . "'></div>";
+                $available_in_stock = $product['product_qty'] > 1 ? "<span class='num-qty-available'>" . $product['product_qty'] . "</span> available in stock" : "Last product in stock";
                 echo "
                             <span class='qty-available'>$available_in_stock</span>
                         </div>
