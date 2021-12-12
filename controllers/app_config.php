d92cad7e953535486ddade4bae98d028b2e6b30c
jacksonemmanuel <71940927+jacksonemmanuel@users.noreply.github.com>
2021-12-12 13:25:25 +0100

    Update app_config.php

--- controllers/app_config.php
+++ controllers/app_config.php
@@ -15,7 +15,7 @@ class Database {
         return pg_fetch(pg_query($this->dbObj, "SELECT * FROM $tableName WHERE $tableName" . "_Id = $id $extra_param"));
     }
     function readAll($tableName, $extra_param = "") {
-        return pg_fetch_all(pg_query($this->dbObj, "SELECT * FROM $tableName $extra_param"));
+        return pg_fetch_all(pg_query($this->dbObj, "SELECT * FROM $tableName $extra_param"), PGSQL_ASSOC);
     }
     function search($tableName, $column, $value, $extra_param = "") {
         return pg_fetch_all(pg_query($this->dbObj, "SELECT * FROM $tableName WEHRE to_tsvector($column) @@ to_tsquery($value) $extra_param"), PGSQL_ASSOC);
